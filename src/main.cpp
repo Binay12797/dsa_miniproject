32c6900c1437fcdb4c70fb074cde3c511ee893e7
Binay Bista <binay.bista.009@gmail.com>
2026-02-22 14:07:09 +0545

    Disk added

--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,21 @@ int main() // codes inside main
         DrawRectangle(100,650,platform_length,40,BLACK); //rectangle for base
 
         int tower_gap=platform_length/4;
-        int tower_x=250;
+        int tower_y=250;
         int tower_height=400;
-        DrawRectangle(tower_gap+75,tower_x,20,tower_height,BLACK); //origin tower
-        DrawRectangle(2*tower_gap+100,tower_x,20,tower_height,BLACK); //mid tower
-        DrawRectangle(3*tower_gap+125,tower_x,20,tower_height,BLACK); //destination tower
+        int tower_width=20;
+
+        int first_x=tower_gap+75;
+        int second_x=2*tower_gap+100;
+        int third_x=3*tower_gap+125;
+
+        DrawRectangle(first_x,tower_y,tower_width,tower_height,BLACK); //origin tower
+        DrawRectangle(second_x,tower_y,tower_width,tower_height,BLACK); //mid tower
+        DrawRectangle(third_x,tower_y,tower_width,tower_height,BLACK); //destination tower
+
+        int disk_radius=100;
+        Rectangle disk = {(float)(first_x - disk_radius + tower_width/2), 650-30 , (float)(2*disk_radius), 30.0f };
+        DrawRectangleRounded(disk ,0.5f ,12 , BLUE);
         
         EndDrawing(); //after enddraing is executed then only all graphics is drawn 
     }
---
+++ src/stack.cpp
@@ -0,0 +1,1 @@
+//each tower uses the concept of stack to solve this puzzle
