8eb36304e6a239833fff4a43778f9dc1cab52572
Binay Bista <binay.bista.009@gmail.com>
2026-02-22 13:46:41 +0545

    platform added

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int width=800, height=800;
+int width=1280, height=800;
 
 int main() // codes inside main
 {
@@ -12,11 +12,21 @@ int main() // codes inside main
 
     while(WindowShouldClose()==false)  //WindowShouldClose is an important function
     {
+        ClearBackground(GREEN); // will clear everything drawin in previous loop
         BeginDrawing();
-        ClearBackground(GREEN); // will clear background every frame and fill with give background colour
-       
+        //drawing part
+
+        int platform_length=1080;
+        DrawRectangle(100,650,platform_length,40,BLACK); //rectangle for base
+
+        int tower_gap=platform_length/4;
+        int tower_x=250;
+        int tower_height=400;
+        DrawRectangle(tower_gap+75,tower_x,20,tower_height,BLACK); //origin tower
+        DrawRectangle(2*tower_gap+100,tower_x,20,tower_height,BLACK); //mid tower
+        DrawRectangle(3*tower_gap+125,tower_x,20,tower_height,BLACK); //destination tower
         
-        EndDrawing(); //after enddraing is executed then only all graphics is drawn
+        EndDrawing(); //after enddraing is executed then only all graphics is drawn 
     }
 
     CloseWindow();
